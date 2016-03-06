ec848125694831b480684067f8c139e8308b94ec
Feng Guo <fengsterooni@gmail.com>
2016-03-06 11:52:01 -0800

    Use IntentService to repalce Service; Parse JSON to get rid of AsyncHTTP

--- app/src/main/java/com/udacity/nanodegrees/service/UpdaterService.java
+++ app/src/main/java/com/udacity/nanodegrees/service/UpdaterService.java
@@ -1,94 +1,123 @@
 package com.udacity.nanodegrees.service;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
 import com.udacity.nanodegrees.data.DegreeContract;
-import com.udacity.nanodegrees.model.NanoDegree;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Service;
-import android.content.ContentProviderOperation;
+import android.app.IntentService;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
-import cz.msebera.android.httpclient.Header;
-
-public class UpdaterService extends Service {
+public class UpdaterService extends IntentService {
+    public final String LOG_TAG = UpdaterService.class.getSimpleName();
     private static final String TAG = "UpdaterService";
-
     public static final String BROADCAST_ACTION_STATE_CHANGE
             = "com.udacity.nanodegrees.intent.action.STATE_CHANGE";
 
-    LocalBroadcastManager broadcastManager;
-    String degreeString = "https://www.udacity.com/public-api/v0/courses?projection=internal";
-
     public UpdaterService() {
+        super(TAG);
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        broadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
-    }
+    protected void onHandleIntent(Intent intent) {
+        final String NANODEGREE_BASE_URL =
+                "https://www.udacity.com/public-api/v0/courses?projection=internal";
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+        try {
+            URL url = new URL(NANODEGREE_BASE_URL);
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        final ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
-        final Uri degreeUri = DegreeContract.DegreeEntry.buildDegreeUri();
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
 
-        cpo.add(ContentProviderOperation.newDelete(degreeUri).build());
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(degreeString, null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                JSONArray degreesJSON = null;
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            String degreesJsonStr = buffer.toString();
+            parseDegreesFromJson(degreesJsonStr);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
                 try {
-                    degreesJSON = response.getJSONArray("degrees");
-
-                    if (degreesJSON == null) throw new JSONException("Invalid JSON Array");
-                    List<NanoDegree> list = NanoDegree.fromJSONArray(degreesJSON);
-
-                    for (NanoDegree degree : list) {
-                        ContentValues values = new ContentValues();
-                        values.put(DegreeContract.DegreeEntry.COLUMN_NAME, degree.getName());
-                        values.put(DegreeContract.DegreeEntry.COLUMN_IMAGE, degree.getImage());
-                        cpo.add(ContentProviderOperation.newInsert(degreeUri).withValues(values).build());
-                    }
-
-                    getContentResolver().applyBatch(DegreeContract.CONTENT_AUTHORITY, cpo);
-                    broadcastManager.sendBroadcast(new Intent(BROADCAST_ACTION_STATE_CHANGE));
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
                 }
             }
+        }
+    }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+    private void parseDegreesFromJson(String jsonStr)
+            throws JSONException {
+
+        final String DEGREES = "degrees";
+        final String TITLE = "title";
+        final String IMAGE = "image";
+
+        try {
+            JSONObject JSON = new JSONObject(jsonStr);
+            JSONArray degreesJSON = JSON.getJSONArray(DEGREES);
+
+            if (degreesJSON == null) throw new JSONException("Invalid JSON Array");
+
+            Vector<ContentValues> cVVector = new Vector<>(degreesJSON.length());
+
+            for (int i = 0; i < degreesJSON.length(); i++) {
+                JSONObject degree = degreesJSON.getJSONObject(i);
+                ContentValues values = new ContentValues();
+                values.put(DegreeContract.DegreeEntry.COLUMN_NAME, degree.getString(TITLE));
+                values.put(DegreeContract.DegreeEntry.COLUMN_IMAGE, degree.getString(IMAGE));
+                cVVector.add(values);
             }
-        });
 
-        return super.onStartCommand(intent, flags, startId);
+            if (cVVector.size() > 0) {
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                cVVector.toArray(cvArray);
+                getContentResolver().bulkInsert(DegreeContract.DegreeEntry.CONTENT_URI, cvArray);
+
+                LocalBroadcastManager.getInstance(getApplicationContext())
+                        .sendBroadcast(new Intent(BROADCAST_ACTION_STATE_CHANGE));
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
     }
 }
